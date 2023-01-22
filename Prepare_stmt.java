5c80204780194cbf5574dba169d7451d1d3aaf7d
disha7555 <119554768+disha7555@users.noreply.github.com>
2023-01-22 14:13:55 +0530

    first commit

--- Prepare_stmt.java
+++ Prepare_stmt.java
@@ -3,11 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package JDBC;
+import java.sql.*;
 /**
  *
  * @author Welcome
  */
-public class Prepare_stmt {
-    
+public class Prepare_stmt 
+{
+    public static void main(String args[]) throws Exception
+    {
+        String urldb = "jdbc:mysql://localhost:3306/Student1";
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn=DriverManager.getConnection(urldb,"root","");
+        if(conn!=null)
+        {
+            
+            System.out.println("connectin successful");
+            String sql="insert into stud1 values(22,'pqr')";
+            PreparedStatement pstmt=conn.prepareStatement(sql);
+            int rows=pstmt.executeUpdate();
+            System.out.println(rows);
+        }
+    }
 }
+
+
